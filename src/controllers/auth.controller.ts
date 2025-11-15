9de6b88253e5382bdf58b7c9c79b9a55002b28f5
Angel santibañez <angelxd0714@gmail.com>
2025-11-14 19:09:16 -0500

    BUILD: SE CONSTRUYE auth y los middlares para auth,role,upload

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -165,8 +165,8 @@ export const requestPasswordReset = async (
 
     const transporter = nodemailer.createTransport({
       host: "smtp.gmail.com",
-      port: 587,
-      secure: false,
+      port: 465,
+      secure: true,
       auth: {
         user: process.env.EMAIL,
         pass: process.env.EMAIL_PASSWORD,
